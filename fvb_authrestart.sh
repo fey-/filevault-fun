767f3aa4432837302ed389816b04219b0418eb96
fey- <matt.carr@utexas.edu>
2014-08-27 16:53:11 -0500

    Update fvb_authrestart.sh

--- fvb_authrestart.sh
+++ fvb_authrestart.sh
@@ -1,4 +1,11 @@
 #!/usr/local/bin/expect -f
+
+# I steal things from the internet and then fail at making them better.
+#
+# Matt Carr
+# matt.carr@utexas.edu
+# 
+
 spawn sudo fdesetup authrestart
 expect ":"
 send "password\n"
