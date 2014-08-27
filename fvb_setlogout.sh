cadadcae9cb7a8ba2ff2df7067e453b5accdd9f5
fey- <matt.carr@utexas.edu>
2014-08-27 16:54:04 -0500

    Update fvb_setlogout.sh

--- fvb_setlogout.sh
+++ fvb_setlogout.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# I steal things from the internet and then fail at making them better.
+#
+# Matt Carr
+# matt.carr@utexas.edu
+#
+
 sudo defaults write com.apple.loginwindow LogoutHook /path/to/scripts/fvb_logout.sh
 
 exit 0
