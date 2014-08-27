66e71883459e7d9e13f4cec326a55c60c276fc40
fey- <matt.carr@utexas.edu>
2014-08-27 16:53:41 -0500

    Update fvb_logout.sh

--- fvb_logout.sh
+++ fvb_logout.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# I steal things from the internet and then fail at making them better.
+#
+# Matt Carr
+# matt.carr@utexas.edu
+#
+
 sudo expect /path/to/scripts/fvb_authrestart.sh
 
 exit 0
